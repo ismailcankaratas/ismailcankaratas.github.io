373f94ad6b69cbf7b1c378a5354a9c3eca609764
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-05-23 22:27:31 +0300

    updated

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./portfolio.css"
 import portfolio from '../../assets/null-portfolio.jpg'
-
+import TrelloClone from "../../assets/trelloClone.png"
 
 const data = [
     {
         id: 1,
         image: portfolio,
         title: "Trello Clone",
+        description: "React ile Trello Clone uygulaması.",
         github: "https://github.com/ismailcankaratas/TrelloClone",
         demo: "https://ismailcankaratas.github.io/TrelloClone/"
     },
@@ -15,6 +16,7 @@ const data = [
         id: 2,
         image: portfolio,
         title: "İftara Nekadar Kaldı?",
+        description: "React ile Türkiye'deki tüm şehirlerin iftara nekadar kaldığını hesaplayan uygulama.",
         github: "https://github.com/ismailcankaratas/iftaraNeKadarKaldi",
         demo: "https://iftaranekadarkaldi.netlify.app/"
     },
@@ -22,6 +24,7 @@ const data = [
         id: 3,
         image: portfolio,
         title: "Blazor Battles",
+        description: ".NET Blazor savaş oyunu.",
         github: "https://github.com/ismailcankaratas/BlazorBattles",
         demo: "https://github.com/ismailcankaratas/BlazorBattles"
     },
@@ -37,13 +40,14 @@ export default function Portfolio() {
             <div className='container portfolio__container'>
 
                 {
-                    data.map(({ id, image, title, github, demo }) => {
+                    data.map(({ id, image, title, description, github, demo }) => {
                         return (
                             <article className='portfolio__item' key={id}>
                                 <div className='portfolio__item-image'>
                                     <img src={image} alt="" />
                                 </div>
                                 <h3>{title}</h3>
+                                <p className='text-light'>{description}</p>
                                 <div className='portfolio__item-cta'>
                                     <a href={github} className='btn' target="_blank">Github</a>
                                     <a href={demo} className='btn btn-primary' target="_blank">Live Demo</a>
