7d0dae7c0c8898ef9e9b4b734b6fa8761adb0fdb
İsmail Can Karataş <ismailcankaratasss@gmail.com>
2024-06-18 22:23:00 +0300

    update

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,34 +1,40 @@
 import React from "react";
 import "./portfolio.css";
-import portfolio from "../../assets/null-portfolio.jpg";
-import SesliAsistanImage from "../../assets/sesli-asistan.png";
 import { BsGithub } from "react-icons/bs";
+
+// Portfolio Images
+import VoiceAssistantImage from "../../assets/portfolio/voice-assistant.png";
+import ChristmasRaffle from "../../assets/portfolio/christmas-raffle.png";
+import IftaraNeKadarKaldi from "../../assets/portfolio/iftaraNeKadarKaldi.png";
 
 const data = [
   {
     id: 1,
-    image: portfolio,
-    title: "Dbcopycat",
+    image: ChristmasRaffle,
+    title: "Christmas Raffle",
+    badges: ["NextJs v3", "TypeScript", "TailwindCSS", "MongoDB"],
     description:
-      "Json verilerinizi bir dosyaya kaydeden ve CRUD işlemlerini gerçekleştirmenizi kolaylaştıran bir JSON Veritabanı.",
-    github: "https://github.com/ismailcankaratas/dbcopycat",
-    demo: "https://ismailcankaratas.github.io/dbcopycat/",
+      "Sistem, çekilise katılanları gelistirdigimiz bir algoritma ile eslestirilmesi ve birbirlerine alacakları sürpriz hediyelerle tanısmasına dayanıyor.",
+    github: "https://github.com/ismailcankaratas/christmas-raffle",
+    demo: "",
   },
   {
     id: 2,
-    image:
-      "https://repository-images.githubusercontent.com/539013996/25dd0874-2203-4686-90fc-bc7be9ccb894",
-    title: "Contrib Chat",
+    image: IftaraNeKadarKaldi,
+    title: "İftara Ne Kadar Kaldi?",
+    badges: ["ReactJS v18", "Redux Toolkit", "Tailwind", "Collect API"],
     description:
-      "Github hesabıyla giriş yaparak. GitHub'da katkıda bulunduğunuz repolarda katkıda bulunanlarla sohbet edebilirsiniz.",
-    github: "https://github.com/ismailcankaratas/contribchat",
-    demo: "https://contribchat.vercel.app/",
+      "Türkiye deki tüm illerin iftar vakitlerine ne kadar kaldığını gösteren proje.",
+    github: "https://github.com/ismailcankaratas/iftaraNeKadarKaldi",
+    demo: "https://ismailcankaratas.com.tr/iftaraNeKadarKaldi/",
   },
   {
     id: 3,
-    image: SesliAsistanImage,
-    title: "Sesli Asistan",
-    description: "Python ve OpenAI ile sesli asistan",
+    image: VoiceAssistantImage,
+    title: "Voice Assistant",
+    badges: ["Python", "OpenAI API"],
+    description:
+      "OpenAI'nin doğal dil işleme yetenekleri ve Python programlama dilinin gücünden faydalanarak geliştirilmiş kullanıcının doğal dilini anlayabilen bir sesli asistandır.",
     github: "https://github.com/ismailcankaratas/voice-assistant",
     demo: "https://www.linkedin.com/feed/update/urn:li:activity:7040641536480452609/",
   },
@@ -41,21 +47,28 @@ export default function Portfolio({ id, navKey, observerRefs }) {
       <h2>Portföy</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, description, github, demo }) => {
+        {data.map(({ id, image, title, badges, description, github, demo }) => {
           return (
             <article className="portfolio__item" key={id}>
               <div className="portfolio__item-image">
                 <img src={image} alt="" />
               </div>
               <h3>{title}</h3>
+              <div className="portfolio__item_badges">
+                {badges.map((badge) => (
+                  <span>{badge}</span>
+                ))}
+              </div>
               <p className="text-light">{description}</p>
               <div className="portfolio__item-cta">
                 <a href={github} className="btn" target="_blank">
                   Github
                 </a>
-                <a href={demo} className="btn btn-primary" target="_blank">
-                  Live Demo
-                </a>
+                {demo && (
+                  <a href={demo} className="btn btn-primary" target="_blank">
+                    Live Demo
+                  </a>
+                )}
               </div>
             </article>
           );
